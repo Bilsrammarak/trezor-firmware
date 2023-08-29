X-oss-byte/Trezor.firmwork

fix(core): display XPUBs in a similar way to addresses
[no changelog]

core/mocks/generated/trezorui2.pyi
@@ -501,8 +501,10 @@ def confirm_reset_device(
 # rust/src/ui/model_tt/layout.rs
 def show_address_details(
     *,
+    title: str,
     address: str,
     case_sensitive: bool,
+    subtitle: str,
     account: str | None,
     path: str | None,
     xpubs: list[tuple[str, str]],
@@ -631,7 +633,7 @@ def show_info(
 
 
 # rust/src/ui/model_tt/layout.rs
-def show_mismatch() -> object:
+def show_mismatch(*, title: str) -> object:
     """Warning modal, receiving address mismatch."""
 
 

core/src/apps/bitcoin/get_public_key.py
@@ -79,9 +79,26 @@ async def get_public_key(
     )
 
     if msg.show_display:
-        from trezor.ui.layouts import show_xpub
+        from trezor.ui.layouts import show_pubkey
+        from .keychain import address_n_to_name
+        from apps.common.paths import address_n_to_str
 
-        await show_xpub(node_xpub, "XPUB")
+        path = address_n_to_str(address_n)
+        account_name = address_n_to_name(coin, address_n, script_type)
+        if account_name is None:
+            account = "Unknown path"
+        elif account_name == "":
+            account = coin.coin_shortcut
+        else:
+            account = f"{coin.coin_shortcut} {account_name}"
+        await show_pubkey(
+            node_xpub,
+            "XPUB",
+            account=account,
+            path=path,
+            mismatch_title="XPUB mismatch?",
+            br_type="show_xpub",
+        )
 
     return PublicKey(
         node=node_type,

tests/input_flows.py
@@ -381,6 +381,32 @@ class InputFlowShowMultisigXPUBs(InputFlowBase):
         self.debug.press_middle()
 
 
+class InputFlowShowXpubQRCode(InputFlowBase):
+    def __init__(self, client: Client):
+        super().__init__(client)
+
+    def input_flow_tt(self) -> GeneratorType:
+        yield
+        self.debug.click(buttons.CORNER_BUTTON, wait=True)
+        # synchronize; TODO get rid of this once we have single-global-layout
+        self.debug.synchronize_at("HorizontalPage")
+
+        self.debug.swipe_left(wait=True)
+        self.debug.swipe_right(wait=True)
+        self.debug.swipe_left(wait=True)
+        self.debug.click(buttons.CORNER_BUTTON, wait=True)
+        self.debug.press_no(wait=True)
+        self.debug.press_no(wait=True)
+        self.debug.press_yes()
+
+    def input_flow_tr(self) -> GeneratorType:
+        br = yield  # paginated data
+        # Confirm - no QR on R
+        for _ in range(br.pages):
+            self.debug.wait_layout()
+            self.debug.press_right()
+
+
 class InputFlowPaymentRequestDetails(InputFlowBase):
     def __init__(self, client: Client, outputs: list[messages.TxOutputType]):
         super().__init__(client)

Bilsrammarak/trezor-firmware

core/mocks/generated/trezorui2.pyi
@@ -215,7 +215,7 @@ def show_passphrase() -> object:
 
 
 # rust/src/ui/model_tr/layout.rs
-def show_mismatch() -> object:
+def show_mismatch(*, title: str) -> object:
     """Warning modal, receiving address mismatch."""
 
 
@@ -501,8 +501,10 @@ def confirm_reset_device(
 # rust/src/ui/model_tt/layout.rs
 def show_address_details(
     *,
+    qr_title: str,
     address: str,
     case_sensitive: bool,
+    details_title: str,
     account: str | None,
     path: str | None,
     xpubs: list[tuple[str, str]],
@@ -631,7 +633,7 @@ def show_info(
 
 
 # rust/src/ui/model_tt/layout.rs
-def show_mismatch() -> object:
+def show_mismatch(*, title: str) -> object:
     """Warning modal, receiving address mismatch."""
 
 

core/src/apps/bitcoin/get_public_key.py
@@ -79,9 +79,28 @@ async def get_public_key(
     )
 
     if msg.show_display:
-        from trezor.ui.layouts import show_xpub
+        from trezor.ui.layouts import show_pubkey
 
-        await show_xpub(node_xpub, "XPUB")
+        from apps.common.paths import address_n_to_str
+
+        from .keychain import address_n_to_name
+
+        path = address_n_to_str(address_n)
+        account_name = address_n_to_name(coin, address_n, script_type)
+        if account_name is None:
+            account = "Unknown account"
+        elif account_name == "":
+            account = coin.coin_shortcut
+        else:
+            account = f"{coin.coin_shortcut} {account_name}"
+        await show_pubkey(
+            node_xpub,
+            "XPUB",
+            account=account,
+            path=path,
+            mismatch_title="XPUB mismatch?",
+            br_type="show_xpub",
+        )
 
     return PublicKey(
         node=node_type,

tests/input_flows.py
@@ -381,6 +381,47 @@ class InputFlowShowMultisigXPUBs(InputFlowBase):
         self.debug.press_middle()
 
 
+class InputFlowShowXpubQRCode(InputFlowBase):
+    def __init__(self, client: Client):
+        super().__init__(client)
+
+    def input_flow_tt(self) -> GeneratorType:
+        br = yield
+        layout = self.debug.wait_layout()
+        if "coinjoin" in layout.title().lower():
+            self.debug.press_yes()
+            br = yield
+
+        self.debug.click(buttons.CORNER_BUTTON, wait=True)
+        # synchronize; TODO get rid of this once we have single-global-layout
+        self.debug.synchronize_at("HorizontalPage")
+
+        self.debug.swipe_left(wait=True)
+        self.debug.swipe_right(wait=True)
+        self.debug.swipe_left(wait=True)
+        self.debug.click(buttons.CORNER_BUTTON, wait=True)
+        self.debug.press_no(wait=True)
+        self.debug.press_no(wait=True)
+        for _ in range(br.pages - 1):
+            self.debug.swipe_up(wait=True)
+        self.debug.press_yes()
+
+    def input_flow_tr(self) -> GeneratorType:
+        br = yield
+        # Go into details
+        self.debug.press_right()
+        # Go through details and back
+        self.debug.press_right()
+        self.debug.press_right()
+        self.debug.press_left()
+        self.debug.press_left()
+        assert br.pages is not None
+        for _ in range(br.pages - 1):
+            self.debug.press_right()
+        # Confirm
+        self.debug.press_middle()
+
+
 class InputFlowPaymentRequestDetails(InputFlowBase):
     def __init__(self, client: Client, outputs: list[messages.TxOutputType]):
         super().__init__(client)